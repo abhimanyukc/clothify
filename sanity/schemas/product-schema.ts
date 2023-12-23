26fa707461f734a36602a2c1c076bd8f54e4dd0c
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-23 10:48:39 +0545

    sanity integn and defining product schema for product input field in sanity studio

--- sanity/schemas/product-schema.ts
+++ sanity/schemas/product-schema.ts
@@ -1,1 +1,75 @@
-export const product = {}
+//using this product object we r going to create schema 
+//and define all of different properties or fields they're called in sanity.
+
+import { defineField, defineType } from "sanity";
+
+//using definetype helper from sanity to wrap our object
+export const product = defineType({
+    name: "product",
+    title: "Products",
+    type: "document",
+    fields: [
+        defineField({
+            name: "name",
+            title: "Name",
+            type: "string",
+        }),
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "name"
+            }
+        },
+        {
+            name: "images",
+            title: "Images",
+            type: "array",
+            of: [{ type: 'image'}]
+        },
+        {
+            name: "categories",
+            title: "Categories",
+            type: "array",
+            of: [{ type: 'string'}]
+
+        },
+        {
+            name: "sizes",
+            title: "Sizes",
+            type: "array",
+            of: [{ type: 'string'}]
+
+        },
+        {
+            name: "colors",
+            title: "Colors",
+            type: "array",
+            of: [{ type: 'string'}]
+
+        },
+        {
+            name: "description",
+            title: "Description",
+             type: 'string'
+
+        },
+        {
+            name: "sku",
+            title: "SKU",
+             type: 'string'
+        },
+        {
+            name: "currency",
+            title: "Currency",
+             type: 'string'
+        },
+        {
+            name: "price",
+            title: "Price",
+             type: 'number'
+
+        },
+    ]
+})
