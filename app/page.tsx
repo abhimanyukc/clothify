3f73794548504a86e63c15310b606fb110cc333b
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-31 18:44:53 +0545

    filtering product with options of each section,checked to checkbox when clicked on label,and changing searchParams according to it,appending multiple option from different section in searchquery ,removing checksearch params url after refresh

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,28 @@ interface Props {
   searchParams: {
     date?: string
     price?: string
+    color?: string
+    category?: string
+    size?: string
   }
 }
 
 export default async function Page({ searchParams}: Props) {
   // console.log(props)
-  const {date = "desc", price} = searchParams
+  //we do want to provide a default value  for date of descending
+  
+  const {date = "desc", price, color, category, size} = searchParams
   const priceOrder = price ? `| order(price ${price})` : ""
   const dateOrder =  date ? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
-
-
+  const productFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+   const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+   
    const products =  await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"]  ${order} {
+    groq`${filter}  ${order} {
         _id,
         _createdAt,
         name,
