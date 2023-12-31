2edcf088698da79edd28a5e2d28da7c9aea8809f
Abhimanyu kc <abhidesign88@gmail.com>
2023-12-31 08:13:58 +0545

    making the image loading experience a bit better by adding a shimmer effect and passing name and price value with currency

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -30,9 +30,17 @@ export function ProductGrid({ products}: Props) {
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:col-span-3 lg:gap-x-8">
       {products.map((product) => (
-        <Link key={product._id} href={`/products/${product.slug}`} className="group text-sm">
+        <Link 
+        key={product._id} 
+        href={`/products/${product.slug}`} 
+        className="group text-sm"
+        >
           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-100 group-hover:opacity-75 dark:border-gray-800">
             <Image
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64
+                (shimmer(225,280)
+                )}`}
               src={urlForImage(product.images[0]).url()}
               alt={product.name}
               width={225}
@@ -40,8 +48,9 @@ export function ProductGrid({ products}: Props) {
               className="h-full w-full object-cover object-center"
             />
           </div>
-          <h3 className="mt-4 font-medium">Name</h3>
-          <p className="mt-2 font-medium">Price</p>
+          <h3 className="mt-4 font-medium">{product.name}</h3>
+          <p className="mt-2 font-medium">{formatCurrencyString({currency: product.currency, 
+            value:product.price })}</p>
         </Link>
       ))}
     </div>
