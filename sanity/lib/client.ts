860750287e748b99ceceb1b502bc511bcceae4b5
Abhimanyu kc <abhidesign88@gmail.com>
2024-01-01 18:21:26 +0545

    removing token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -8,6 +8,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-  "skM7sZS0xlhRNsEXHDOflO7KhkJUkQUyAiy20gjYeQceFfgge3O4yXH1MWz5za56MUt5Jdby10C2CUeqjv7Gbu3Tg5dF62nOMPUa9v7cY3egsp75ATO1P2DiDATPnm7JH8KZ2hCEuTao9ObI86AJgJReXERdE0yOMxbwxbZlDrXWK2wWxYGK",
+  
 });
